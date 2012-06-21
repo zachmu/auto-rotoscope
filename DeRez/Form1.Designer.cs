cc8511aec13fb131df05c7c02acd5329b692d7e9
Zach Musgrave <zach.musgrave@gmail.com>
2012-06-21 16:51:04 -0700

    First draft of de-rezzing image processor

---
+++ DeRez/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace DeRez {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if ( disposing && (components != null) ) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.src = new System.Windows.Forms.PictureBox();
+            this.dest = new System.Windows.Forms.PictureBox();
+            this.pixelSize = new System.Windows.Forms.TextBox();
+            this.darkThreshold = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.targetWidth = new System.Windows.Forms.TextBox();
+            this.targetHeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.src)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dest)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 467);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // src
+            // 
+            this.src.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.src.Location = new System.Drawing.Point(25, 12);
+            this.src.Name = "src";
+            this.src.Size = new System.Drawing.Size(339, 324);
+            this.src.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.src.TabIndex = 1;
+            this.src.TabStop = false;
+            // 
+            // dest
+            // 
+            this.dest.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dest.Location = new System.Drawing.Point(379, 12);
+            this.dest.Name = "dest";
+            this.dest.Size = new System.Drawing.Size(345, 324);
+            this.dest.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.dest.TabIndex = 2;
+            this.dest.TabStop = false;
+            // 
+            // pixelSize
+            // 
+            this.pixelSize.Location = new System.Drawing.Point(157, 390);
+            this.pixelSize.Name = "pixelSize";
+            this.pixelSize.Size = new System.Drawing.Size(100, 20);
+            this.pixelSize.TabIndex = 3;
+            this.pixelSize.TextChanged += new System.EventHandler(this.pixelSize_TextChanged);
+            // 
+            // darkThreshold
+            // 
+            this.darkThreshold.Location = new System.Drawing.Point(157, 416);
+            this.darkThreshold.Name = "darkThreshold";
+            this.darkThreshold.Size = new System.Drawing.Size(100, 20);
+            this.darkThreshold.TabIndex = 4;
+            this.darkThreshold.TextChanged += new System.EventHandler(this.darkThreshold_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Select input file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 390);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Pixel size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dark threshold";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // targetWidth
+            // 
+            this.targetWidth.Location = new System.Drawing.Point(568, 389);
+            this.targetWidth.Name = "targetWidth";
+            this.targetWidth.Size = new System.Drawing.Size(100, 20);
+            this.targetWidth.TabIndex = 8;
+            this.targetWidth.TextChanged += new System.EventHandler(this.targetWidth_TextChanged);
+            // 
+            // targetHeight
+            // 
+            this.targetHeight.Location = new System.Drawing.Point(568, 415);
+            this.targetHeight.Name = "targetHeight";
+            this.targetHeight.Size = new System.Drawing.Size(100, 20);
+            this.targetHeight.TabIndex = 9;
+            this.targetHeight.TextChanged += new System.EventHandler(this.targetHeight_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(478, 389);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Target width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(478, 423);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Target height";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 579);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.targetHeight);
+            this.Controls.Add(this.targetWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.darkThreshold);
+            this.Controls.Add(this.pixelSize);
+            this.Controls.Add(this.dest);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.src);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.src)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dest)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox src;
+        private System.Windows.Forms.PictureBox dest;
+        private System.Windows.Forms.TextBox pixelSize;
+        private System.Windows.Forms.TextBox darkThreshold;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox targetWidth;
+        private System.Windows.Forms.TextBox targetHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DeRez/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DeRez {
+    public partial class Form1 : Form {
+        private Image _image;
+        private int _pixelSize = 4;
+        private float _threshold = .2f;
+        private int _targetWidth = 64;
+        private int _targetHeight = 128;
+        private Rectangle _cropArea;
+
+        Boolean bHaveMouse = false;
+        Point ptOriginal = new Point();
+        Point ptLast = new Point(); 
+
+        public Form1() {
+            _cropArea = new Rectangle(0, 0, _targetWidth, _targetHeight);
+            InitializeComponent();
+            src.Resize += (sender, args) => dest.Location = new Point(src.Location.X + src.Width + 5, dest.Location.Y);
+            src.Paint += SrcPicBox_Paint;
+            src.MouseDown += SrcPicBox_MouseDown;
+            src.MouseMove += SrcPicBox_MouseMove;
+            src.MouseUp += SrcPicBox_MouseUp;
+
+            pixelSize.Text = _pixelSize.ToString();
+            darkThreshold.Text = _threshold.ToString();
+            targetHeight.Text = _targetHeight.ToString();
+            targetWidth.Text = _targetWidth.ToString();
+        }
+
+        private void SrcPicBox_MouseDown(object sender, MouseEventArgs e) {
+            // Make a note that we "have the mouse". 
+            bHaveMouse = true;
+
+            // Store the "starting point" for this rubber-band rectangle. 
+            ptOriginal.X = e.X;
+            ptOriginal.Y = e.Y;
+
+            // Special value lets us know that no previous 
+            // rectangle needs to be erased. 
+
+            ptLast.X = -1;
+            ptLast.Y = -1;
+
+            _cropArea = new Rectangle(new Point(e.X, e.Y), new Size());
+        }
+
+        private void SrcPicBox_MouseUp(object sender, MouseEventArgs e) {
+            // Set internal flag to know we no longer "have the mouse". 
+            bHaveMouse = false;
+
+            // If we have drawn previously, draw again in that spot 
+            // to remove the lines. 
+            if ( ptLast.X != -1 ) {
+                Point ptCurrent = new Point(e.X, e.Y);
+            }
+
+            // Set flags to know that there is no "previous" line to reverse. 
+            ptLast.X = -1;
+            ptLast.Y = -1;
+            ptOriginal.X = -1;
+            ptOriginal.Y = -1;
+
+            DeRez();
+        }
+
+        private void SrcPicBox_MouseMove(object sender, MouseEventArgs e) {
+            Point ptCurrent = new Point(e.X, e.Y);
+
+            // If we "have the mouse", then we draw our lines. 
+            if ( bHaveMouse ) {
+                // If we have drawn previously, draw again in 
+                // that spot to remove the lines. 
+
+                // Update last point. 
+                ptLast = ptCurrent;
+
+                // Draw new lines. 
+
+                // e.X - _cropArea.X; 
+                // normal 
+                if ( e.X > ptOriginal.X && e.Y > ptOriginal.Y ) {
+                    _cropArea.Width = e.X - ptOriginal.X;
+
+                    // e.Y - _cropArea.Height; 
+                    _cropArea.Height = e.Y - ptOriginal.Y;
+                } else if ( e.X < ptOriginal.X && e.Y > ptOriginal.Y ) {
+                    _cropArea.Width = ptOriginal.X - e.X;
+                    _cropArea.Height = e.Y - ptOriginal.Y;
+                    _cropArea.X = e.X;
+                    _cropArea.Y = ptOriginal.Y;
+                } else if ( e.X > ptOriginal.X && e.Y < ptOriginal.Y ) {
+                    _cropArea.Width = e.X - ptOriginal.X;
+                    _cropArea.Height = ptOriginal.Y - e.Y;
+
+                    _cropArea.X = ptOriginal.X;
+                    _cropArea.Y = e.Y;
+                } else {
+                    _cropArea.Width = ptOriginal.X - e.X;
+
+                    // e.Y - _cropArea.Height; 
+                    _cropArea.Height = ptOriginal.Y - e.Y;
+                    _cropArea.X = e.X;
+                    _cropArea.Y = e.Y;
+                }
+
+                // adjust the width and height of the crop area to match the target
+                float ratio = (float) _targetHeight / _targetWidth;
+                float cropRatio = (float) _cropArea.Height / _cropArea.Width;
+                if ( Math.Abs(ratio - cropRatio) > .1 ) {
+                    _cropArea.Height = (int) (_cropArea.Width * ratio);
+                }
+
+                src.Refresh();
+            }
+        }
+
+        private Bitmap CropSource() {
+            
+            //Prepare a new Bitmap on which the cropped image will be drawn 
+            Bitmap sourceBitmap = new Bitmap(src.Image, src.Width, src.Height);            
+            Bitmap b;
+            try {
+                b = new Bitmap(_cropArea.Width, _cropArea.Height);
+            } catch ( ArgumentException ) {
+                return sourceBitmap;
+            }
+
+            using ( Graphics g = Graphics.FromImage(b) ) {
+
+                //Draw the image on the Graphics object with the new dimesions 
+                g.DrawImage(sourceBitmap, new Rectangle(0, 0, b.Width, b.Height),
+                            _cropArea, GraphicsUnit.Pixel);
+
+                //Good practice to dispose the System.Drawing objects when not in use. 
+                sourceBitmap.Dispose();
+
+                return b;
+            }
+        } 
+
+        private void SrcPicBox_Paint(object sender, PaintEventArgs e) {
+            if ( _cropArea != null ) {
+                Pen drawLine = new Pen(Color.Black);
+                drawLine.DashStyle = DashStyle.Dash;
+                e.Graphics.DrawRectangle(drawLine, _cropArea);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            _image = Image.FromFile(path);
+            src.Image = _image;
+            //_cropArea = new Rectangle(0, 0, _image.Width, _image.Height);
+            DeRez();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            DeRez();
+        }
+
+        private void DeRez() {
+            if ( _image != null ) {
+                Bitmap bitmap = CropSource();
+                int outputWidth = bitmap.Width / _pixelSize;
+                int outputHeight = bitmap.Height / _pixelSize;
+                Bitmap output = new Bitmap(outputWidth, outputHeight);
+                for ( int y = 0; y < outputHeight; y++ ) {
+                    for ( int x = 0; x < outputWidth; x++ ) {
+                        int black = 0;
+                        for ( int yy = 0; y * _pixelSize + yy < bitmap.Height && yy < _pixelSize; yy++ ) {
+                            for ( int xx = 0; x * _pixelSize + xx < bitmap.Width && xx < _pixelSize; xx++ ) {
+                                Color pixel = bitmap.GetPixel(x * _pixelSize + xx, y * _pixelSize + yy);
+                                if ( IsPixelDark(pixel) ) {
+                                    black++;
+                                }
+                            }
+                        }
+                        if ( black > (_pixelSize * _pixelSize) / 2 ) {
+                            output.SetPixel(x, y, Color.Black);
+                        } else {
+                            output.SetPixel(x, y, Color.White);
+                        }
+                    }
+                }
+
+                output = ResizeImage(output, _targetWidth, _targetHeight);
+                dest.Image = output;
+            }
+        }
+
+        private bool IsPixelDark(Color pixel) {
+            return pixel.GetBrightness() < _threshold;
+        }
+
+        private void pixelSize_TextChanged(object sender, EventArgs e) {
+            int parse;
+            if ( Int32.TryParse(pixelSize.Text, out parse) ) {
+                _pixelSize = parse;
+                DeRez();
+            }
+        }
+
+        private void darkThreshold_TextChanged(object sender, EventArgs e) {
+            float parse;
+            if ( float.TryParse(darkThreshold.Text, out parse) ) {
+                _threshold = parse;
+                DeRez();
+            }
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(System.Drawing.Image image, int width, int height) {
+            //a holder for the result
+            Bitmap result = new Bitmap(width, height);
+
+            //use a graphics object to draw the resized image into the bitmap
+            using ( Graphics graphics = Graphics.FromImage(result) ) {
+                //set the resize quality modes to high quality
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighSpeed;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+                //draw the image into the target bitmap
+                graphics.DrawImage(image, 0, 0, result.Width, result.Height);
+            }
+
+            //return the resulting bitmap
+            return result;
+        }
+
+        private void targetWidth_TextChanged(object sender, EventArgs e) {
+            int parse;
+            if (Int32.TryParse(targetWidth.Text, out parse)) {
+                _targetWidth = parse;
+                DeRez();
+            }
+        }
+
+        private void targetHeight_TextChanged(object sender, EventArgs e) {
+            int parse;
+            if ( Int32.TryParse(targetHeight.Text, out parse) ) {
+                _targetHeight = parse;
+                DeRez();
+            }
+        }
+
+    }
+}
